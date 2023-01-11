082321c177a5ae49c85bca6c8f6d1f72513c6afc
wotres <wotres@naver.com>
2023-01-11 23:04:37 +0900

    [ADD] scroll and gasp

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// lodash 를 이용 _.throttle(함수, 시간)
+window.addEventListener('scroll', _.throttle(function() {
+  if (window.scrollY > 500) {
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none',
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block',
+    })
+  }
+}, 300));
