e05f2cee08f020c8b54b2f5c0acd9be37160be03
wotres <wotres@naver.com>
2023-01-14 22:13:29 +0900

    [ADD] notice & swiper slide

--- js/main.js
+++ js/main.js
@@ -39,3 +39,46 @@ fadeEls.forEach(function(fadeEl, index) {
     opacity: 1,
   })
 })
+
+// css 에서 띄워쓰기는 후손 선택자
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: {
+    delay: 2500
+  },
+  loop: true // 4 -> 1 번 슬라이드 반복 가능
+});
+
+new Swiper('.promotion .swiper', {
+  direction: 'horizontal', // default 라 삭제 가능
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백 10px
+  centeredSlides: true, // 처음 슬라이드가 가운데로
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+const promotionToggleBtnText = promotionToggleBtn.querySelector('.material-symbols-outlined')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+    promotionToggleBtnText.textContent = 'download';
+  } else {
+    promotionEl.classList.remove('hide');
+    promotionToggleBtnText.textContent = 'upload';
+  }
+})
